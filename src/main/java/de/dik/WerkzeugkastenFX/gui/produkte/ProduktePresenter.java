638dfedb31e0bc391236fb3224e40a4ccaec9b0b
seriouslyEnter <seriouslyEnter@gmail.com>
2018-06-08 22:46:03 +0200

    Revert "Code und Dateien aufgeräumt"
    This reverts commit d7c4edcd79f8998e9c213d600998802603e6d51e.

--- src/main/java/de/dik/WerkzeugkastenFX/gui/produkte/ProduktePresenter.java
+++ src/main/java/de/dik/WerkzeugkastenFX/gui/produkte/ProduktePresenter.java
@@ -1,6 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package de.dik.WerkzeugkastenFX.gui.produkte;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXToggleButton;
 import de.dik.WerkzeugkastenFX.gui.wkKlassen.IstSoll;
 import de.dik.WerkzeugkastenFX.gui.wkKlassen.WkButton;
 import java.net.URL;
@@ -14,12 +20,113 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
+/**
+ *
+ * @author gu35nxt
+ */
 public class ProduktePresenter implements Initializable {
 
     @FXML
     private ScrollPane produkteSP;
+
+    @FXML
+    private AnchorPane produkteAP;
+
     @FXML
     private GridPane produkteGP;
+
+    @FXML
+    private JFXButton bt11;
+
+    @FXML
+    private JFXButton bt12;
+
+    @FXML
+    private JFXButton bt13;
+
+    @FXML
+    private JFXButton bt14;
+
+    @FXML
+    private JFXButton bt15;
+
+    @FXML
+    private JFXButton bt21;
+
+    @FXML
+    private JFXButton bt22;
+
+    @FXML
+    private JFXButton bt23;
+
+    @FXML
+    private JFXButton bt24;
+
+    @FXML
+    private JFXButton bt25;
+
+    @FXML
+    private JFXButton bt31;
+
+    @FXML
+    private JFXButton bt32;
+
+    @FXML
+    private JFXButton bt33;
+
+    @FXML
+    private JFXButton bt34;
+
+    @FXML
+    private JFXButton bt35;
+
+    @FXML
+    private JFXButton bt41;
+
+    @FXML
+    private JFXButton bt42;
+
+    @FXML
+    private JFXButton bt43;
+
+    @FXML
+    private JFXButton bt44;
+
+    @FXML
+    private JFXButton bt45;
+
+    @FXML
+    private JFXButton bt51;
+
+    @FXML
+    private JFXButton bt52;
+
+    @FXML
+    private JFXButton bt53;
+
+    @FXML
+    private JFXButton bt54;
+
+    @FXML
+    private JFXButton bt55;
+
+    @FXML
+    private JFXButton bt61;
+
+    @FXML
+    private JFXButton bt62;
+
+    @FXML
+    private JFXButton bt63;
+
+    @FXML
+    private JFXButton bt64;
+
+    @FXML
+    private JFXButton bt65;
+
+    @FXML
+    private JFXToggleButton IstSollTB;
 
     private WkButton[][] wkButtonGrid = new WkButton[6][5];
 
@@ -28,11 +135,15 @@ public class ProduktePresenter implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.resources = resources;
-        //Im übergeordnetem AnchorPane positionieren
         AnchorPane.setTopAnchor(produkteSP, 0.0);
         AnchorPane.setLeftAnchor(produkteSP, 0.0);
         AnchorPane.setRightAnchor(produkteSP, 0.0);
         AnchorPane.setBottomAnchor(produkteSP, 0.0);
+
+        //Ist/Soll ToggleButton für den Start auf false(Ist) setzen
+        //.isSelected funktioniert beim stat nicht. Es bleibt grau.
+        IstSollTB.setSelected(true);
+        IstSollTB.setSelected(false);
 
         JFXButton jfxButtonForComparisson = new JFXButton();
         int row = 0;
@@ -43,7 +154,13 @@ public class ProduktePresenter implements Initializable {
                 JFXButton nextButton = new JFXButton();
                 nextButton = (JFXButton) nextNode;
                 wkButtonGrid[row][column] = new WkButton(nextButton, row, column, IstSoll.NEUTRAL);
-                //Spalten dann Zeilen hochzählen. Von Spalten 0-4(1-5)
+//                JFXButton nextButton = (JFXButton) nextNode;
+////                System.out.println("zähler: " + nextButton.getId());
+//                wkButtonGrid[row][column].setButton(nextButton);
+//                wkButtonGrid[row][column].setIstSoll(0);
+//                wkButtonGrid[row][column].setRow(row);
+//                wkButtonGrid[row][column].setRow(column);
+//                //Spalten dann Zeilen hochzählen. Von Spalten 0-4(1-5)
                 column++;
                 if (column > 4) {
                     row++;
@@ -56,10 +173,10 @@ public class ProduktePresenter implements Initializable {
     @FXML
     public void btClick(ActionEvent event) {
         System.out.println("de.dik.WerkzeugkastenFX.gui.produkte.ProduktePresenter.bt11Click()");
+
         JFXButton button = (JFXButton) event.getSource();
 
         //TODO: Case nur ein grün und rot pro Zeile
-        
         //WkButton anhand von button finden.
         //Damit die Eigenschaften von WkButton geändert werden können.
         //Und damit die Farbe von JFXButton geändert werden kann.        
@@ -68,44 +185,70 @@ public class ProduktePresenter implements Initializable {
             for (int column = 0; column < wkButtonsRow.length; column++) {
                 JFXButton wkButton = wkButtonsRow[column].getButton();
                 if (wkButton.equals(button)) {
+                    
+                    //TODO: Toggle Button entfernen, der ist nicht mehr nötig und stört sogar!
+                    
+                    
                     //Wenn Button gefunden IstSoll setzen
                     //IstSoll ändern vom geklickted Button
                     //wenn Button neutral und Toggle rot -> rot
-                    switch (wkButtonsRow[column].getIstSoll()) {
-                        case NEUTRAL:
-                            //ist
-                            wkButtonsRow[column].setIstSoll(IstSoll.IST);
-                            //Farbe rot
-                            button.setStyle("-fx-background-color: rgba(255, 0, 0, 0.5);");
-                            //wenn Button rot und Toggle grün -> rot und grün   
-                            break;
-                        case IST:
-                            //ist und soll
-                            wkButtonsRow[column].setIstSoll(IstSoll.ISTundSOLL);
-                            //Farbe grün
-                            button.setStyle("-fx-background-color: linear-gradient(from 40% 40% to 60% 60%, #ff0000, #558c5a); -fx-opacity: 0.5");
-                            //wenn Button gradient und Toggle grün -> grün
-                            break;
-                        case ISTundSOLL:
-                            //soll
-                            wkButtonsRow[column].setIstSoll(IstSoll.SOLL);
-                            //Farbe grün
-                            button.setStyle("-fx-background-color: rgba(85, 140, 90, 0.5);");
-                            //wenn Button rot und Toggle rot -> neutral
-                            break;
-                        case SOLL:
-                            //neutral
-                            wkButtonsRow[column].setIstSoll(IstSoll.NEUTRAL);
-                            //Farbe rot
-                            button.setStyle("-fx-background-color: transparent;");
-                            break;
-                        default:
-                            System.out.println("Wenn hier dann Fehler");
-                            break;
+                    if (wkButtonsRow[column].getIstSoll().equals(IstSoll.NEUTRAL)) {
+                        //ist
+                        wkButtonsRow[column].setIstSoll(IstSoll.IST);
+                        //Farbe rot
+                        button.setStyle("-fx-background-color: rgba(255, 0, 0, 0.5);");
+                        //Flip ToogleButton
+                        IstSollTB.setSelected(true);
+                    //wenn Button rot und Toggle grün -> rot und grün   
+                    } else if (wkButtonsRow[column].getIstSoll().equals(IstSoll.IST)) {
+                        //ist und soll
+                        wkButtonsRow[column].setIstSoll(IstSoll.ISTundSOLL);
+                        //Farbe grün
+                        button.setStyle("-fx-background-color: linear-gradient(from 40% 40% to 60% 60%, #ff0000, #558c5a); -fx-opacity: 0.5");
+                        //dont Flip ToogleButton
+//                        IstSollTB.setSelected(false);
+                    //wenn Button gradient und Toggle grün -> grün
+                    } else if (wkButtonsRow[column].getIstSoll().equals(IstSoll.ISTundSOLL)) {
+                        //soll
+                        wkButtonsRow[column].setIstSoll(IstSoll.SOLL);
+                        //Farbe grün
+                        button.setStyle("-fx-background-color: rgba(85, 140, 90, 0.5);");
+                        //Flip ToogleButton
+                        IstSollTB.setSelected(false);
+                    //wenn Button rot und Toggle rot -> neutral
+                    } else if (wkButtonsRow[column].getIstSoll().equals(IstSoll.SOLL)){
+                        //neutral
+                        wkButtonsRow[column].setIstSoll(IstSoll.NEUTRAL);
+                        //Farbe rot
+                        button.setStyle("-fx-background-color: transparent;");
+                        //Flip ToogleButton
+                        IstSollTB.setSelected(false);
+                    } else {
+                        System.out.println("Wenn hier dann Fehler");
                     }
+                break;
                 }
             }
         }
+        
+
+//        //Farbe ändern vom geklickted Button
+//        if (IstSollTB.selectedProperty().getValue() != true) {
+//            //rot
+//            button.setStyle("-fx-background-color: rgb(255, 0, 0, 0.5); ");
+//            IstSollTB.setSelected(true);
+//        } else {
+//            //grün
+//            button.setStyle("-fx-background-color: rgb(85, 140, 90, 0.5); ");
+//            IstSollTB.setSelected(false);
+//        }
+
         System.out.println(button.getId());
+    }
+
+    @FXML
+    void IstSollTBAction(ActionEvent event) {
+        System.out.println("fire");
+        System.out.println(IstSollTB.selectedProperty().toString());
     }
 }
---
+++ src/main/java/de/dik/WerkzeugkastenFX/gui/wkKlassen/WkButton.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dik.WerkzeugkastenFX.gui.wkKlassen;
+
+import com.jfoenix.controls.JFXButton;
+import java.util.Objects;
+
+/**
+ *
+ * @author gu35nxt
+ */
+public class WkButton {
+    private JFXButton button;
+    private Integer row;    //1-6
+    private Integer column; //1-5
+    private IstSoll istSoll; // 0(not selected) 0(ist) 1(soll)
+    
+        public WkButton(JFXButton button, Integer row, Integer column) {
+        this.button = button;
+        this.row = row;
+        this.column = column;
+        this.istSoll = IstSoll.NEUTRAL;
+    }
+
+    public WkButton(JFXButton button, Integer row, Integer column, IstSoll istSoll) {
+        this.button = button;
+        this.row = row;
+        this.column = column;
+        this.istSoll = istSoll;
+    }
+
+    public JFXButton getButton() {
+        return button;
+    }
+
+    public void setButton(JFXButton button) {
+        this.button = button;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public IstSoll getIstSoll() {
+        return istSoll;
+    }
+
+    public void setIstSoll(IstSoll istSoll) {
+        this.istSoll = istSoll;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.button);
+        hash = 97 * hash + Objects.hashCode(this.row);
+        hash = 97 * hash + Objects.hashCode(this.column);
+        hash = 97 * hash + Objects.hashCode(this.istSoll);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WkButton other = (WkButton) obj;
+        if (!Objects.equals(this.button, other.button)) {
+            return false;
+        }
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (this.istSoll != other.istSoll) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "WkButton{" + "button=" + button + ", row=" + row + ", column=" + column + ", istSoll=" + istSoll + '}';
+    }
+
+}
